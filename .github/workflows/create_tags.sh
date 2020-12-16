35338c0ea1cd9fa3af5eb9baeff8d4952b21c325
Ayaka Tajiri <44262215+ayaka-tajiri@users.noreply.github.com>
2020-12-16 23:11:56 +0900

    Update create_tags.sh

--- .github/workflows/create_tags.sh
+++ .github/workflows/create_tags.sh
@@ -11,9 +11,11 @@ CURRENT_TAG_NAME=$(echo "$CURRENT_TAG_BODY" | jq --raw-output '.tag_name')
 VERSIONS=$(echo $CURRENT_TAG_NAME | tr "." " ")
 
 if [ `echo $BRANCH | grep Bugfix` ]; then
-  VERSIONS[2]=$(( VERSIONS[2]+1 ))
+  NUMBER=${VERSIONS[2]}
+  VERSIONS[2]=$(( NUMBER+1 ))
 elif [ `echo $BRANCH | grep Feature` ]; then
-  VERSIONS[1]=$(( VERSIONS[1]+1 ))
+  NUMBER=${VERSIONS[1]}
+  VERSIONS[1]=$(( NUMBER+1 ))
 else
   echo "Not a proper branch name."
   exit 0
