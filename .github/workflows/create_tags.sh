2f4f4b3b264646959a4c8cf9218fffa5ddb2025a
Ayaka Tajiri <44262215+ayaka-tajiri@users.noreply.github.com>
2020-12-16 22:28:38 +0900

    Update create_tags.sh

--- .github/workflows/create_tags.sh
+++ .github/workflows/create_tags.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
 echo "run script"
+
+# get pull request number
+BRANCH=$GITHUB_HEAD_REF
+AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+CURRENT_TAG_BODY=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${GITHUB_API_URL}/${GITHUB_REPOSITORY}/releases/latest")
+CURRENT_TAG_NAME=$(echo "$CURRENT_TAG_BODY" | jq --raw-output '.[] | {tag_name: .tag_name} | @base64')
+
+VERSIONS=$(echo $CURRENT_TAG_NAME | tr "." " ")
+
+if [ `echo $branch | grep Bugfix` ]; then
+  VERSIONS[2]=$VERSIONS[2]+1
+elif [ echo $branch | grep Feature ]; then
+  VERSIONS[1]=$VERSIONS[1]+1
+else
+  echo "Not a proper branch name."
+  exit 0
+fi
+
+RESULTS="$(IFS="."; echo "${VERSIONS[*]}")"
+
+echo $RESULTS
