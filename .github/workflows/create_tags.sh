296bfa8add76834e001c1da1b3a32f50dd1444db
Ayaka Tajiri <44262215+ayaka-tajiri@users.noreply.github.com>
2020-12-16 23:00:57 +0900

    Update create_tags.sh

--- .github/workflows/create_tags.sh
+++ .github/workflows/create_tags.sh
@@ -7,22 +7,17 @@ BRANCH=$GITHUB_HEAD_REF
 API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 CURRENT_TAG_BODY=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/releases/latest")
-echo $CURRENT_TAG_BODY
 CURRENT_TAG_NAME=$(echo "$CURRENT_TAG_BODY" | jq --raw-output '.tag_name')
-echo $CURRENT_TAG_NAME
 VERSIONS=$(echo $CURRENT_TAG_NAME | tr "." " ")
-for value in $VERSIONS
-do
-  echo ">[$value]"
-done
 
 if [ `echo $BRANCH | grep Bugfix` ]; then
-  VERSIONS[2]=$VERSIONS[2]+1
+  VERSIONS[2]=${VERSIONS[2]}+1
 elif [ `echo $BRANCH | grep Feature` ]; then
-  VERSIONS[1]=$VERSIONS[1]+1
+  VERSIONS[1]=${VERSIONS[1}+1
 else
   echo "Not a proper branch name."
   exit 0
 fi
 
+RESULTS="$(IFS="."; echo "${list[*]}")"
 echo $RESULTS
