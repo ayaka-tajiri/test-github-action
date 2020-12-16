b9d4cac22cedaa8a1324a155ee71c701d8f337d5
Ayaka Tajiri <44262215+ayaka-tajiri@users.noreply.github.com>
2020-12-16 23:52:11 +0900

    Update create_tags.sh

--- .github/workflows/create_tags.sh
+++ .github/workflows/create_tags.sh
@@ -8,13 +8,14 @@ API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 CURRENT_TAG_BODY=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/releases/latest")
 CURRENT_TAG_NAME=$(echo "$CURRENT_TAG_BODY" | jq --raw-output '.tag_name')
+declare -a VERSIONS=()
 VERSIONS=$(echo $CURRENT_TAG_NAME | tr "." " ")
 NUMBER0=${VERSIONS[0]}
 echo $NUMBER0
 NUMBER1=${VERSIONS[1]}
-echo NUMBER1
+echo $NUMBER1
 NUMBER2=${VERSIONS[2]}
-echo NUMBER2
+echo $NUMBER2
 
 if [ `echo $BRANCH | grep Bugfix` ]; then
   NUMBER2=$(( $NUMBER2+1 ))
