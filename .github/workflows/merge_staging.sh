3da743b567e188746bd387d40d92fba912f3df56
ayaka-tajiri <ayaka0727soudaidou39@gmail.com>
2020-12-17 18:52:48 +0900

    fix

--- .github/workflows/merge_staging.sh
+++ .github/workflows/merge_staging.sh
@@ -3,6 +3,8 @@
 API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+baseBranch=$(jq --raw-output .pull_request.base.ref "$GITHUB_EVENT_PATH")
+headBranch=$(jq --raw-output .pull_request.head.ref "$GITHUB_EVENT_PATH")
 body=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/pulls/${number}/reviews?per_page=100")
 reviews=$(echo "$body" | jq --raw-output '.[] | {state: .state} | @base64')
 
@@ -16,18 +18,13 @@ for r in $reviews; do
   fi
 done
 
-#curl -sSL \
-#    -H "${AUTH_HEADER}" \
-#    -H "Content-Type: application/json" \
-#    -X POST \
-#    -d "{\"base\":\"staging\", \"head\":\"${GITHUB_HEAD_REF}\"}" \
-#    "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/merges"
-
-cat $GITHUB_EVENT_PATH
-curl --location --request POST 'https://api.github.com/repos/ayaka-tajiri/test-github-action/merges' \
-  -H "${AUTH_HEADER}" \
-  --header 'Content-Type: application/json' \
-  -d "{\"base\":\"staging\", \"head\":\"${GITHUB_HEAD_REF}\"}"
+mergeStaging=$(curl -sSL \
+    -H "${AUTH_HEADER}" \
+    -H "${API_HEADER}" \
+    -X POST \
+    -d "{\"base\":\"staging\", \"head\":\"${headBranch}\"}" \
+    "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/merges")
+echo $mergeStaging
 
 #if [[ "$approvals" -ge "$APPROVALS" ]] && [[ "$GITHUB_BASE_REF" -ge "main" ]]; then
 #  mergeStaging=$(curl -sSL \
